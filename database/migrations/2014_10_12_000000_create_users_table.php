1e013bee9fe17d6f01d60c7bef48baa3c0c1c5aa
LINTANGDIKA <lintangdik0@gmail.com>
2022-07-23 23:36:37 +0700

    Feat[LD] add tabel

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('nama');
             $table->integer('overall');
             $table->string('email')->unique();
-            $table->string('role');
+            $table->string('rank');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
