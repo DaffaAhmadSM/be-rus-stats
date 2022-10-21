f02e53d238fda4a6e7caa6e0f0de86ab8115dd1a
DaffaAhmadSM <daffamubaroqi321@gmail.com>
2022-10-21 13:36:58 +0700

    feat[DA] add rakit skill

--- app/Http/Controllers/DivisiController.php
+++ app/Http/Controllers/DivisiController.php
@@ -16,20 +16,39 @@ class DivisiController extends Controller
     public function divisiCreate(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            "nama" => 'required|string',
-            "department_id" => 'required|integer'
+            "divisi" => 'array|required',
+            "divisi.nama" => 'required|string',
+            "divisi.department_id" => 'required|integer',
+            "skill" => 'array|required',
+            "skill.*" => 'array|required',
+            "skill.*.skill_id" => 'required|integer',
+            "skill.*.sub_skill_id" => 'required|integer',
         ]);
         if ($validator->fails()) {
-            return response()->json(["Error" => $validator->errors()->first()], 400);
+            return response()->json([$validator->errors()], 400);
         }
-        if (divisi::where('nama', $request->nama)->where('department_id', $request->department_id)->exists()) {
+        if (divisi::where('nama', $request->divisi["nama"])->where('department_id', $request->divisi["department_id"])->exists()) {
             return response()->json(["Error" => "Divisi already exists"], 400);
         }
+
+        
+
         try {
             $datacreate = divisi::create([
-                "nama" => $request -> nama,
-                "department_id" => $request -> department_id
+                "nama" => $request -> divisi["nama"],
+                "department_id" => $request -> divisi["department_id"]
             ]);
+        $divisi_skill_subskill = [];
+
+        foreach ($request->skill as $skill) {
+            $divisi_skill_subskill[] = [
+                "skill_id" => $skill["skill_id"],
+                "sub_skill_id" => $skill["sub_skill_id"],
+                "divisi_id" => $datacreate->id
+            ];
+        }
+
+        DivisiSkillSubskill::insert($divisi_skill_subskill);
         }
         catch (Exception $e) {
             return response()->json(["Error" => $e->getMessage()], 500);
