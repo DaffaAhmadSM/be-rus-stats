f3499f3f75fb6cf0e734bb70878f90eafdd550a4
DaffaAhmadSM <daffamubaroqi321@gmail.com>
2022-12-10 10:09:23 +0700

    add cv

--- app/Http/Controllers/CVController.php
+++ app/Http/Controllers/CVController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Education;
+use App\Models\User;
+use App\Models\Project;
 use App\Models\Language;
+use App\Models\Education;
 use App\Models\LanguageUser;
 use App\Models\SoftwareUser;
 use Illuminate\Http\Request;
@@ -14,22 +16,34 @@ class CVController extends Controller
         return 'all';
     }
     public function cvUserId($id){
-        $languageModel = LanguageUser::where('user_id', $id)->with('language');
-        $softwareModel = SoftwareUser::where('user_id', $id)->with('software');
+        $user = User::find($id);
+        $languageModel = LanguageUser::where('user_id', $id)->with('language')->get();
+        $softwareModel = SoftwareUser::where('user_id', $id)->with('software')->get();
         $educationUser = Education::where('user_id', $id)->get();
+        $projectUser = Project::where('user_id', $id)->get();
         $softwareUser = [];
         $languageUser = [];
-        foreach($softwareModel->get() as $s){
+        foreach($softwareModel as $s){
             $softwareUser[] = $s->software;
         }
-        foreach($languageModel->get() as $s){
+        foreach($languageModel as $s){
             $languageUser[] = $s->language;
         }
-        // printf($softwareUser);
+
+        foreach($softwareUser as $s){
+            if($s->image == null){
+                $s->image = asset('storage/software_images/default.png');
+            }
+            $s->image = asset('storage/software_images'.$s->image);
+        }
+
+
         return response()->json([
-            'education' => $educationUser,
-            'langugage' => $languageUser,
-            'software' => $softwareUser
+            'user' => $user,
+            'education' => $educationUser,  
+            'language' => $languageUser,
+            'software' => $softwareUser,
+            'project' => $projectUser,
         ],200);
     }
 }
